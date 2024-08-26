fd20f4bceb9985c2d1a31b54dd0f44d49ca01790
Bisola <abizmichelle@gmail.com>
2024-08-26 09:45:20 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import PlayersList from "./components/PlayersList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FIFA Player Cards</h1>
+     <PlayersList />
+    </div>
+  );
+}
+
+export default App;
